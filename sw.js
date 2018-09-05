dc633b166c4aa2a735d3f427173aa8265c75d1da
Genghis Bai <bai.genghis@gmail.com>
2018-09-04 20:24:58 -0700

    modified manifest

--- sw.js
+++ sw.js
@@ -6,9 +6,18 @@ self.addEventListener("install", function(event) {
         "./js/main.js",
         "./css/style.css",
         "./index.html",
-        "./data.json"
-        // "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css",
-        // "https://use.fontawesome.com/releases/v5.2.0/css/all.css"
+        "./data.json",
+        "./images/icons/icon-72x72.png",
+        "./images/icons/icon-96x96.png",
+        "./images/icons/icon-128x128.png",
+        "./images/icons/icon-144x144.png",
+        "./images/icons/icon-152x152.png",
+        "./images/icons/icon-192x192.png",
+        "./images/icons/icon-384x384.png",
+        "./images/icons/icon-512x512.png",
+        "./images/eloquence.png",
+        "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css",
+        "https://use.fontawesome.com/releases/v5.2.0/css/all.css"
       ]);
     })
   );
